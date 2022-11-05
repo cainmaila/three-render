e9d5b1b68e880cbbea126b166d083ec76e3aaf13
cain <cainmaila@gmail.com>
2022-11-05 22:29:24 +0800

    新增模型1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -16,6 +16,11 @@ export default function Home() {
               廢棄工廠(GLTF)
             </a>
           </li>
+          <li>
+            <a href="/viewer/man" target="viewer_gltf">
+              藝術品(GLTF) 203M
+            </a>
+          </li>
         </ol>
       </nav>
     </>
