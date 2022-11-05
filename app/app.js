649e742b0c6a1f7a357839dce1dda8590d563852
cain <cainmaila@gmail.com>
2022-11-05 22:38:37 +0800

    新增模型render

--- app/app.js
+++ app/app.js
@@ -56,4 +56,5 @@ if (process.env.NODE_ENV === 'production') {
   //TODO:暫時寫死
   openPage(`http://localhost:${PORT}/render/tci`);
   openPage(`http://localhost:${PORT}/render/gltf`);
+  openPage(`http://localhost:${PORT}/render/man`);
 }
