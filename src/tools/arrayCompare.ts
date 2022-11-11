99e4c858e01a1aa54aeade8c9fe515887f7f9eec
cain <cainmaila@gmail.com>
2022-11-11 13:58:01 +0800

    比較出 arrB 來過過濾 arrA，列出 B 沒有的值

---
+++ src/tools/arrayCompare.ts
@@ -0,0 +1,11 @@
+/**
+ * 比較出 arrB 來過過濾 arrA，列出 B 沒有的值
+ * @param arrA
+ * @param arrB
+ * @returns
+ */
+export const difference = <T>(arrA: [T], arrB: [T]) => {
+  return arrA.filter((e) => {
+    return arrB.indexOf(e) === -1;
+  });
+};
