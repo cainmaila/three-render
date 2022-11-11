2981fdde0e22bb5c1ad567879ed7ac525231d0b1
cain <cainmaila@gmail.com>
2022-11-11 13:58:20 +0800

    srcObject 播放物件

---
+++ src/components/VideoOb.tsx
@@ -0,0 +1,17 @@
+import { useEffect, useRef } from 'react';
+
+export interface Prop {
+  stream: MediaStream | undefined;
+}
+
+export default ({ stream }: Prop) => {
+  const vdoRef = useRef<HTMLVideoElement>(document.createElement('video'));
+  useEffect(() => {
+    if (vdoRef.current && stream) {
+      vdoRef.current.srcObject = stream;
+      vdoRef.current.autoplay = true;
+    }
+  }, [stream]);
+
+  return <video ref={vdoRef}></video>;
+};
