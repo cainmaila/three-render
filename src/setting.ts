a79a464e3fb8ad3f9d03ab20aea2cef5ea6ee5d4
cain <cainmaila@gmail.com>
2022-12-16 15:46:14 +0800

    clear

--- src/setting.ts
+++ src/setting.ts
@@ -11,26 +11,3 @@ export interface I_ModelMeta {
   type: MODEL_TYPE;
   tag?: string;
 }
-
-export default {
-  tci: {
-    path: '/model/TciBio_20220311.fbx',
-    initPosition: [-3000, 5000, 8000],
-    type: 'fbx',
-  },
-  gltf: {
-    path: '/model/warehouse/scene.gltf',
-    initPosition: [0, 0, -20],
-    type: 'gltf',
-  },
-  man: {
-    path: '/model/man/scene.gltf',
-    initPosition: [0, 0, 5],
-    type: 'gltf',
-  },
-  csc: {
-    path: '/model/CSC.fbx',
-    initPosition: [3000, 2000, 5],
-    type: 'fbx',
-  },
-};
